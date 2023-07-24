4825ee489b0fbcbc71abe592c27cc44d28b452ec
Brett Albright <balbright@learningfuze.com>
2023-07-24 21:00:51 +0000

    completed update to code for deployment

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,24 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table "public"."users" (
+  "userId"         serial,
+  "username"       text not null,
+  "hashedPassword" text not null,
+  "createdAt"      timestamptz(6) not null default now(),
+  primary key ("userId"),
+  unique ("username")
+);
+
+create table "public"."entries" (
+  "entryId"  serial,
+  "userId"   int not null,
+  "title"    text not null,
+  "notes"    text not null,
+  "photoUrl" text not null,
+  primary key ("entryId")
+);
+
+ALTER TABLE ONLY "public"."entries"
+  ADD CONSTRAINT user_id_fkey FOREIGN KEY ("userId") REFERENCES "public"."users"("userId") ON UPDATE CASCADE ON DELETE RESTRICT;
